a6d8e242aac76a06cb6beec06db7364c94cb098a
tct123 <42028373+tct123@users.noreply.github.com>
2022-07-14 19:01:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ import kivy
 import kivymd
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
+from kivy.uix.screenmanager import Screen
+from kivymd.uix.button import MDRectangleFlatButton
 
 class MainApp(MDApp):
     def build(self):
-        return MDLabel(text="Hello, World", halign="center")
+        screen = Screen()
+        screen.add_widget(MDRectangleFlatButton(
+            text="Hello World",
+            pos_hint={"center_x": 0.5, "center_y":0.5},
+            on_press=exit,
+            )
+        )
+        return screen
+        #return MDLabel(text="Hello, World", halign="center")
 
 MainApp().run()
 # request_url = https://www.gelbeseiten.de/Suche/{"job"}/{"city"}
