3e01ebdec179d7637435a8b641e7af54f3f76eb0
tct123 <42028373+tct123@users.noreply.github.com>
2022-07-14 19:27:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 from kivy.uix.screenmanager import Screen
 from kivymd.uix.button import MDRectangleFlatButton
+from kivymd.uix.textfield import MDTextField
 
 class MainApp(MDApp):
     def build(self):
@@ -13,10 +14,12 @@ class MainApp(MDApp):
         screen.add_widget(MDRectangleFlatButton(
             text="Hello World",
             pos_hint={"center_x": 0.5, "center_y":0.5},
-            on_press=exit,
-            )
+            on_press=exit,),
+        screen.add_widget(MDTextField(
+            hint_text = "No helper text",
+            ),
         )
-        screen.add_widget()
+    )
         return screen
         #return MDLabel(text="Hello, World", halign="center")
 
