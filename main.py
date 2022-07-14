f1d7be4c5e4b769607c43f53f2d2629d521a95c0
tct123 <42028373+tct123@users.noreply.github.com>
2022-07-14 18:40:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 import kivy
 import kivymd
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
 
+class MainApp(MDApp):
+    def build(self):
+        return MDLabel(text="Hello, World", halign="center")
+
+MainApp().run()
 # request_url = https://www.gelbeseiten.de/Suche/{"job"}/{"city"}
 # plzurl = https://home.meinestadt.de/deutschland/plz-6
