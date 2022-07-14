48a3df464357787ab42e2233c8cd428d71dafae7
tct123 <42028373+tct123@users.noreply.github.com>
2022-07-14 16:46:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import kivy
+import kivymd
 
 # request_url = https://www.gelbeseiten.de/Suche/{"job"}/{"city"}
 # plzurl = https://home.meinestadt.de/deutschland/plz-6
