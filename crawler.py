a4a208679ceb9b6de43c9dd28169967d8f890cf6
tct123 <42028373+tct123@users.noreply.github.com>
2022-08-01 20:50:50 +0200

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 job = input()
-zipcode = input()
+zipcode = input() #read from .csv file
 cityname = input()
 url = f"https://www.gelbeseiten.de/suche/{job}/{zipcode} {cityname}"
 print(requests.get(url))
